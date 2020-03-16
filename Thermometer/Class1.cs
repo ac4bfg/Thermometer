02a18faa059bebe60f646aeacc1bfabcf9f56a6e
Andre <ac4bfg@gmail.com>
2020-03-16 14:48:27 +0700

    Add project files.

---
+++ Thermometer/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Thermometer
+{
+    class Thermometer
+    {
+        public Double result;
+   
+        public void celciusToFahrenheit(Double celcius) 
+        {
+            result = (celcius * 9 / 5) + 32;
+        }
+
+        public void fahrenheitToCelcius(int fahrenheit) {
+            result = (fahrenheit - 32)* 5/9;
+        }
+
+        public void celciusToKelvin(float kelvin) {
+            result = kelvin + 275.15;
+        }
+    }
+}
---
+++ Thermometer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Thermometer
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            Thermometer thermometer = new Thermometer();
+            thermometer.celciusToFahrenheit(32);
+            Console.WriteLine("hasil : " + thermometer.result);
+
+            thermometer.fahrenheitToCelcius(140);
+            Console.WriteLine("hasil : " + thermometer.result);
+
+            thermometer.celciusToKelvin(3);
+            Console.WriteLine("hasil : " + thermometer.result);
+        }
+    }
+}
